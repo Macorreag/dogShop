2a34df77fcc78a7c253a5781b44eff1177a9357a
Miller Correa <macorreag@unal.edu.co>
2017-12-10 21:52:04 -0500

    Inicia implementación de mmap

--- Server/p2-dogServer.c
+++ Server/p2-dogServer.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
@@ -8,148 +9,253 @@
 
 #define CANT 2
 
+unsigned long head[10]={0,0,0,0,0,0,0,0,0,0};  //Dimencion de nuestra tabla Hash cabezas
+unsigned long tail[10]={0,0,0,0,0,0,0,0,0,0};  //Dimencion de nuestra tabla Hash colas 
+
+
 struct dogType
 {
 	char nombre[32];
 	char animal[32];
-	struct dogType *next;
+	unsigned long previus;
+	unsigned long next;
 };
 
-struct celdas
-{
-	struct dogType p[10];
+    int fid,status;
+    FILE *fp;		/*Puntero para crear el archivo*/
+    caddr_t mmap_ptr; /*Puntero cabezera mmap*/
+	
+	
+
+
+
+unsigned long send(long action ){
+
+	switch(action) {
+	      case -1 :
+		fseek(fp,0,SEEK_SET);/* enviamos el puntero al Inicio*/	
+		return ftell(fp);
+		break;
+	      case -2:
+		fseek(fp,0,SEEK_END);/* enviamos el puntero al final */	
+		return ftell(fp);
+		break;
+	      default :
+		fseek(fp,action,SEEK_SET);/* enviamos el puntero a posicion dada en 
+					bytes contada desde el inicio */	
+		return ftell(fp);
+		break;	
+	}
+
+}
+
+struct dogType *searchDog(unsigned long location){
+/*Encuentra y devuelve  el perro en la localizacion "location"*/
+
+    
+    
+  unsigned long x=(unsigned long) (mmap_ptr + location); /*Movemos a la posicion del perro */
+	
+	
+  struct dogType *dog = (struct dogType*) x;/*Casteamos para acceder al perro de forma facil */
+
+    return dog;
+
+
+} 
+
+unsigned long * viewLocationHead(long Hash ){
+
+    unsigned long aux = (unsigned long ) mmap_ptr + (8*Hash);
+	unsigned long *point = (unsigned long *) aux;   
+    return point;
+
+} 
+unsigned long * viewLocationTail(long Hash ){
+
+    unsigned long aux = (unsigned long ) mmap_ptr + (8*Hash) + sizeof(head);
+	unsigned long * point = (unsigned long *) aux;   
+    return point;
+
+} 
+void updateHeadHash(long rank,unsigned long adress){
+	/*actualiza la tabla de punteros Cabeza del Hash con la ubicacion del hash"rank "*/
+	
+	*(mmap_ptr+rank*8)=adress;	
+
+
+}
+void updateTailHash(long rank,unsigned long adress){
+	/*actualiza la tabla de punteros del Hash con la ubicacion del hash(rank)*/
+	
+	*(mmap_ptr+rank*8+(sizeof(head)))=adress;	
+
+
+}
+
+
+
+long numberDog(unsigned long adress){
+
+    
+
+
+}
+
+int  inDog(){   /*agregar NUEVO perro */
+
+	/*Recoleccion e ingreso de una mascota*/
+	struct dogType dog;
+		
+	/*Recolección y validación de Datos */
+	strcpy(dog.nombre,"Boss");
+	strcpy(dog.animal,"perro");
+
+	
+	/*obtener el hash */
+	long hash=0; /*obtuvimos el hash cero llamando a function hash */		
+	
+       
+			if(*viewLocationTail(hash) == 0 ){
+			    
+				
+			    /*PRIMER Perro que ingresa*/			
+				updateHeadHash(hash,send(-2));/*ActualizoCabeza*/				
+				updateTailHash(hash,send(-2));	/*ActualizoCola*/
+				dog.next=0;
+				dog.previus=0;
+				
+				
+					
+			}else{	
+			
+			    /*SEGUNDO o MAYOR Perro que ingresa*/
+			    searchDog(*viewLocationTail(hash))->next=send(-2);/*Actualizacion apuntador que lo contiene*/
+			    dog.previus=*viewLocationTail(hash);
+			    updateTailHash(hash,send(-2));	/*ActualizoCola*/
+				dog.next=0;
+				
+			}
+		
+		/*Guardo el perro en el archivo*/
+		fwrite(&dog, sizeof(struct dogType),1, fp);
+	    return 1;	/*validar correcto 1*/
+		
 };
 
-static void
-error(char *mesg){
+
+
+int  deleteDog(unsigned long posicionMove,struct dogType dog,caddr_t mmap_ptr){
+	//if(posicionMove == &dog){
+		/*actualizacion*/	
+			
+	//}else{
+		/*Movimiento perro*/
+		//unsigned long x=(unsigned long) mmap_ptr + 160;	
+		send(posicionMove-(unsigned long)mmap_ptr);
+		//struct dogType perro = (struct dogType*) posicionMove;
+		fwrite(&dog, sizeof(struct dogType),1, fp);
+	//}	
+
+
+
+}
+
+int  moveDog(unsigned long posicionMove,struct dogType dog,caddr_t mmap_ptr){
+	//if(posicionMove == &dog){
+		/*actualizacion*/	
+			
+	//}else{
+		/*Movimiento perro*/
+		//unsigned long x=(unsigned long) mmap_ptr + 160;	
+		send(posicionMove-(unsigned long)mmap_ptr);
+		//struct dogType perro = (struct dogType*) posicionMove;
+		fwrite(&dog, sizeof(struct dogType),1, fp);
+	//}	
+
+
+
+}
+
+
+static void error(char *mesg){
   perror(mesg);
-  //exit(2);
+  
 }
 
-static void
-show_usage(const char *prog_name, const char *mesg ){
-  fprintf(stderr, "Error: %s\n"
-	  "Usage is:\n%s fname\n\tWhere fname is a file name to use\n",
-	  mesg, prog_name);
-}
 
-int
-main(int argc, char *argv[]){
 
-  FILE *fp;
 
-  int fid, child, status;
-  pid_t wait_status;
-  caddr_t mmap_ptr;
-  char buffer[80];
+int main(int argc, char *argv[]){
 
-  char *file="com.dat";
+  char *file = "dataDogs.dat";	/*Nombre del archivo para la  comunicacion*/	
+  	
+  fp= fopen(file, "rb+");	/*Apertura de Archivo !debe existir! lectura 	
+	escritura en binario*/
 
-/*  if (argc != 2){
-    show_usage(argv[0], "Wrong number of parameters");
-    //exit(1);
-  }*/
-  /* Create a new file for read/write access with permissions restricted
-     to owner rwx access only */
+  fid = fileno(fp);		/*Descriptor del archivo*/
 
-  //fid = open(file, O_RDWR | O_EXCL, (mode_t) 0755 );
-
-  fp= fopen("com.dat", "rb+");
-  fid = fileno(fp);
-
-  if (fid < 0){
+  if (fid < 0){			/*i fileno no pudo crear descriptor retorna 					-1*/	
     fprintf(stderr,"Bad Open of file <%s>\n", file);
     error("Failed to open mmap file, QUIT!");
   }
-/*
-  status = ftruncate(fid, sizeof(buffer)); // make the file the buffer size
-  if (status){
-      fprintf(stderr,"Could not ftruncate(%d, %lu) = %d\n", fid,
-	      sizeof(buffer), status );
-      error("Bad Ftruncate");
-  }
-*/
-  child = fork();
-  if (child == -1){
-    error("Could not fork, QUIT!");
-  } else if (child == 0){ /* This is the child process */
-    /* Notice that the child uses the write system call in this version */
 
-    //sprintf(buffer, "Welcome to the Jungle!");
+    fwrite(head, sizeof(head), 1, fp );
+	fwrite(tail, sizeof(tail), 1, fp );/*Escribimos nuestra tabla hash	en el archivo */
 
-    struct celdas *d;
-    struct dogType perrito2;
-    strcpy(d->p[1].nombre, " theBIg");
-    strcpy(d->p[1].nombre, " gatita ");
+ fclose(fp);
 
+    fp= fopen(file, "ab+");	/*Apertura de Archivo !debe existir! lectura 	
+	escritura en binario*/
 
-   strcpy(d->p[0].nombre, " sasha ");
-    strcpy(d->p[0].animal, " perrito ");
-   /*
-    d->p[0].next = malloc( sizeof( struct dogType ) );
-    *d->p[0].next = perrito2;
-   */
-   fwrite(d, sizeof(struct celdas), 1, fp);
-
-   /*
-    sprintf(buffer, "Hola Mundo...");
-    status = write(fid, buffer, sizeof(buffer));
-    if (status < 0){
-      error("Bad Child Write, Quit!");
-    }
-    status = close(fid); // close the mmap file
-
-    if (status == -1){
-      error("Bad Child Close, Quit!");
-    }*/
-    sleep(2);
-  } else { /* This is the parent Process */
-    sleep(1);
-    /* allocate a shared memory region using mmap which can be inherited
-       by child processes */
-    mmap_ptr = mmap((caddr_t) 0,   /* Memory Location, 0 means O/S chooses */
-		    sizeof(struct celdas),/* How many bytes to mmap */
+  fid = fileno(fp);		/*Descriptor del archivo*/
+ mmap_ptr = mmap((caddr_t) 0,   /* Memory Location, 0 means O/S chooses */
+		    160 ,/* How many bytes to mmap */
 		    PROT_READ | PROT_WRITE, /* Read and write permissions */
 		    MAP_SHARED,    /* Accessible by another process */
 		    fid,           /* which file is associated with mmap */
 		    (off_t) 0);    /* Offset in page frame */
+    
+    
+    
+    
+    
+    
+    
+    
     if (mmap_ptr == MAP_FAILED){
       error("Parent Memory Map Failed, QUIT!");
     }
-    printf("Parent's mmap_ptr = %lx\n", (unsigned long) mmap_ptr);
 
-	struct celdas *c;
-	c = (struct celdas*) mmap_ptr;
-	printf("Parent got the message: %s\n", c->p[0].nombre);
-	//printf("Parent got the message: %s\n", c->p[0].animal);
-	//printf("Parent got the message: %s\n", c->p[0].nextnombre);
+    
 
-    sleep(1);
+	
+    
+    inDog();
+    inDog();
+    inDog();
+    
+    
+        
 
-    status = munmap(mmap_ptr, sizeof(struct celdas));
+    
+
+
     if (status == -1){
       error("Bad munmap, QUIT!");
     }
 
-    fclose(fp);
-    /*
-    status = close(fid); // close the mmap file
-    if (status == -1){
-      error("Bad Parent Close, Quit!");
-    }
-    */
-    /*
-    //unlink (i.e. remove) the mmap file
-    status = unlink(file);
-    if (status == -1){
-      error("Bad unlink, QUIT!");
-    }
-    wait_status = wait( &status );
-    if (wait_status == -1){
-      error("Bad Wait, QUIT!");
-    }*/
 
+
+
+
+
+ fclose(fp);
+
+//inDog(fp,,-1);
+      return 0;
+ 
   }
 
-  return 0;
-}
+
+
